526d564cd8f147c4aeb43f047df07f438b2d6cac
Jatinder Kumar Chaurasia <32101330+JatinderKumarChaurasia@users.noreply.github.com>
2019-02-08 17:54:14 +0530

    Add files via upload

--- static/admin/scripts/main.js
+++ static/admin/scripts/main.js
@@ -1,1 +1,7 @@
-abc
+var id = document.getElementById('click1');
+id.addEventListener('click',changecolor,false);
+
+function changecolor(){
+    var id = document.getElementById('click1');
+    id.style.backgroundColor="black";
+}
