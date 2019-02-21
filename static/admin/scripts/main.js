0b67d53f20924dbf49f6822071883a64f1896293
Jatinder Kumar Chaurasia <32101330+JatinderKumarChaurasia@users.noreply.github.com>
2019-02-21 17:37:45 +0530

    Add files via upload
    Adding Welcome Login and adding checkbox replacing buttons

--- static/admin/scripts/main.js
+++ static/admin/scripts/main.js
@@ -5,3 +5,17 @@ function changecolor(){
     var id = document.getElementById('click1');
     id.style.backgroundColor="black";
 }
+
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+name.addEventListener(listener);
+btn1.style.backgroundColor="gray";
+btn2.style.backgroundColor="gray";
+function btn1listener(){
+    btn1.style.backgroundColor="blue";
+    btn2.style.backgroundColor="gray";
+}
+function btn2listener(){
+    btn1.style.backgroundColor="gray";
+    btn2.style.backgroundColor="blue";
+}
